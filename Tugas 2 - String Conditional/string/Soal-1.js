21d1d58628c3446404f3f24ef856c26228f5086b
NofathZ <nofath86@gmail.com>
2021-03-23 23:40:43 +0700

    mengerjakan tugas harian 2

---
+++ Tugas 2 - String Conditional/string/Soal-1.js
@@ -0,0 +1,16 @@
+var word = 'JavaScript'; 
+var second = 'is'; 
+var third = 'awesome'; 
+var fourth = 'and'; 
+var fifth = 'I'; 
+var sixth = 'love'; 
+var seventh = 'it!';
+
+word += " " + second + " ";
+word += third + " ";
+word += fourth + " ";
+word += fifth + " ";
+word += sixth + " ";
+word += seventh;
+
+console.log(word)
---
+++ Tugas 2 - String Conditional/string/Soal-4.js
@@ -0,0 +1,13 @@
+var sentence2 = 'wow JavaScript is so cool'; 
+
+var firstWord2 = sentence2.substring(0, 3);
+var secondWord2 = sentence2.substring(4, 14);
+var thirdWord2 = sentence2.substring(15, 17);
+var fourthWord2 = sentence2.substring(18, 20);
+var fifthWord2 = sentence2.substring(21, 26);
+
+console.log('First Word: ' + `${firstWord2}, with length: ${firstWord2.length}`);
+console.log('Second Word: ' + `${secondWord2}, with length: ${secondWord2.length}`);
+console.log('Third Word: ' + `${thirdWord2}, with length: ${thirdWord2.length}`);
+console.log('Fourth Word: ' + `${fourthWord2}, with length: ${fourthWord2.length}`);
+console.log('Fifth Word: ' + `${fifthWord2}, with length: ${fifthWord2.length}`);
